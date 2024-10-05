41bacb98cfa5807f48c08df08af6dbdf811e1111
Hich2000 <33690542+Hich2000@users.noreply.github.com>
2024-10-05 11:45:19 +0200

    added simple audio permission request

--- app/src/main/java/com/hich2000/tagcapella/MainActivity.kt
+++ app/src/main/java/com/hich2000/tagcapella/MainActivity.kt
@@ -3,8 +3,14 @@ package com.hich2000.tagcapella
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.io.path.Path
+import kotlin.io.path.listDirectoryEntries
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.activity.result.contract.ActivityResultContracts
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +22,29 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+        // Register the permissions callback, which handles the user's response to the
+        // system permissions dialog. Save the return value, an instance of
+        // ActivityResultLauncher. You can use either a val, as shown in this snippet,
+        // or a lateinit var in your onAttach() or onCreate() method.
+        val requestPermissionLauncher =
+            registerForActivityResult(
+                ActivityResultContracts.RequestPermission()
+            ) {
+            }
+
+
+        val audioPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_MEDIA_AUDIO)
+
+        if (audioPermission != PackageManager.PERMISSION_GRANTED) {
+            requestPermissionLauncher.launch(
+                Manifest.permission.READ_MEDIA_AUDIO)
+        }
+
+
+        var path = Path("/storage/emulated/0/Music").listDirectoryEntries()
+        path.listIterator().forEach { println(it) }
+
     }
 }
