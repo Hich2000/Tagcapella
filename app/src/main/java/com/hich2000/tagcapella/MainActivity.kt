34b73a5ecb5ab910394867d665c02f0cf9234a61
Hich2000 <33690542+Hich2000@users.noreply.github.com>
2024-10-13 14:35:55 +0200

    mainactivity is now compose and I got a viewmodel for the playback service working

--- app/src/main/java/com/hich2000/tagcapella/MainActivity.kt
+++ app/src/main/java/com/hich2000/tagcapella/MainActivity.kt
@@ -1,49 +1,99 @@
 package com.hich2000.tagcapella
 
 import android.Manifest
-import android.content.Intent
+import android.app.Application
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.hich2000.tagcapella.music_player.MusicPlayerActivity
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.ContextCompat
+import androidx.media3.session.MediaController
+import com.hich2000.tagcapella.music_list.ui.ui.theme.TagcapellaTheme
+import com.hich2000.tagcapella.music_player.MusicPlayerViewModel
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
+
+    private lateinit var requestPermissionLauncher: ActivityResultLauncher<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
-        // Register the permissions callback, which handles the user's response to the
-        // system permissions dialog. Save the return value, an instance of
-        // ActivityResultLauncher. You can use either a val, as shown in this snippet,
-        // or a lateinit var in your onAttach() or onCreate() method.
-        val requestPermissionLauncher =
-            registerForActivityResult(
-                ActivityResultContracts.RequestPermission()
-            ) { isGranted: Boolean ->
-                if (isGranted) {
-                    val intent = Intent(this, MusicPlayerActivity::class.java)
-                    startActivity(intent)
-                } else {
-                    val intent = Intent(this, PermissionRequestActivity::class.java)
-                    startActivity(intent)
+        requestPermissions()
+
+        setContent {
+            TagcapellaTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Surface(
+                        modifier = Modifier.padding(innerPadding)
+                    ) {
+                        TagcapellaApp()
+                    }
                 }
             }
+        }
+    }
 
-//        val audioPermission =
-//            ContextCompat.checkSelfPermission(this, Manifest.permission.READ_MEDIA_AUDIO)
 
-        requestPermissionLauncher.launch(
-            Manifest.permission.READ_MEDIA_AUDIO
-        )
+    private fun requestPermissions() {
+        // Initialize the permission launcher
+        requestPermissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (!isGranted) {
+                Toast.makeText(this, "App required media permissions.", Toast.LENGTH_SHORT).show()
+            }
+        }
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            // Check if the permission is already granted
+            if (ContextCompat.checkSelfPermission(
+                    this, Manifest.permission.READ_MEDIA_AUDIO
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                requestPermissionLauncher.launch(Manifest.permission.READ_MEDIA_AUDIO)
+            }
+        } else {
+            if (ContextCompat.checkSelfPermission(
+                    this, Manifest.permission.READ_EXTERNAL_STORAGE
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                requestPermissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+            }
+        }
+    }
+
+
+    @Composable
+    fun TagcapellaApp() {
+
+        val context = LocalContext.current
+        val mediaViewModel = MusicPlayerViewModel(context.applicationContext as Application)
+        var mediaController: MediaController? = null
+
+        // Use the state variable to determine if the MediaController is initialized
+        val isMediaControllerInitialized by mediaViewModel.isMediaControllerInitialized
+
+        Surface(modifier = Modifier.fillMaxSize()) {
+            if (isMediaControllerInitialized) {
+                mediaController = mediaViewModel.mediaController
+                Text(text = "Media Controller Initialized!")
+            } else {
+                CircularProgressIndicator()
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/hich2000/tagcapella/music_player/MusicPlayerViewModel.kt
@@ -0,0 +1,89 @@
+package com.hich2000.tagcapella.music_player
+
+import android.app.Application
+import android.content.ComponentName
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.media3.common.MediaItem
+import androidx.media3.common.MediaMetadata
+import androidx.media3.common.Player
+import androidx.media3.session.MediaController
+import androidx.media3.session.SessionToken
+import com.google.common.util.concurrent.MoreExecutors
+import kotlinx.coroutines.launch
+import java.util.concurrent.ExecutionException
+import kotlin.io.path.Path
+import kotlin.io.path.isDirectory
+import kotlin.io.path.isRegularFile
+import kotlin.io.path.listDirectoryEntries
+import kotlin.io.path.nameWithoutExtension
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+
+class MusicPlayerViewModel(application: Application) : AndroidViewModel(application) {
+
+    private lateinit var _mediaController: MediaController // Nullable MediaController
+    val mediaController: MediaController
+        get() = _mediaController
+
+    // State to indicate if the MediaController is initialized
+    private val _isMediaControllerInitialized = mutableStateOf(false)
+    val isMediaControllerInitialized: State<Boolean> get() = _isMediaControllerInitialized
+
+    init {
+        initializeMediaController()
+    }
+
+
+    fun initializeMediaController() {
+        viewModelScope.launch {
+            val sessionToken =
+                SessionToken(
+                    getApplication(),
+                    ComponentName(getApplication(), PlaybackService::class.java)
+                )
+            val controllerFuture =
+                MediaController.Builder(getApplication(), sessionToken).buildAsync()
+            controllerFuture.addListener(
+                {
+                    try {
+                        _mediaController = controllerFuture.get()
+                        _mediaController.repeatMode = Player.REPEAT_MODE_ALL
+                        _mediaController.addMediaItems(getInitialPlaylist())
+                        _mediaController.prepare()
+                        _isMediaControllerInitialized.value = true // Update the loading state
+                    } catch (e: ExecutionException) {
+
+                    }
+                },
+                MoreExecutors.directExecutor()
+            )
+        }
+    }
+
+    private fun getInitialPlaylist(): MutableList<MediaItem> {
+        val playlist = mutableListOf<MediaItem>()
+
+        val path = Path("/storage/emulated/0/Music").listDirectoryEntries()
+        path.listIterator().forEach {
+            if (!it.isDirectory() && it.isRegularFile()) {
+
+                val mediaItem = MediaItem.Builder()
+                    .setMediaId(it.toString())
+                    .setUri(it.toString())
+                    .setMediaMetadata(
+                        MediaMetadata.Builder()
+                            .setTitle(it.nameWithoutExtension)
+                            .setDisplayTitle(it.nameWithoutExtension)
+                            .build()
+                    )
+                    .build()
+
+                playlist.add(mediaItem)
+            }
+        }
+
+        return playlist
+    }
+
+}
