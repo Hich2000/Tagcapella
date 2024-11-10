70217470bf0cd01be6d7ec11a4748f8bdbcaae51
Hich2000 <33690542+Hich2000@users.noreply.github.com>
2024-11-10 17:42:03 +0100

    removed old SongListViewModel.kt in favor of the new repository approach

--- app/src/main/java/com/hich2000/tagcapella/CompositionLocals.kt
+++ app/src/main/java/com/hich2000/tagcapella/CompositionLocals.kt
@@ -1,8 +1,6 @@
 package com.hich2000.tagcapella
 
 import androidx.compose.runtime.compositionLocalOf
-import com.hich2000.tagcapella.music_player.SongListViewModel
 import com.hich2000.tagcapella.tags.TagViewModel
 
-val LocalSongListViewModel = compositionLocalOf<SongListViewModel> { error("SongListViewModel not provided") }
 val LocalTagViewModel = compositionLocalOf<TagViewModel> { error("TagViewMdoel not provided") }
