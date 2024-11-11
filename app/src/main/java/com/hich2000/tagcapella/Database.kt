72b9244f152e04ed748a383bd420d39cc1acae4e
Hich2000 <33690542+Hich2000@users.noreply.github.com>
2024-11-11 15:40:23 +0100

    extracted database into it's own thing

---
+++ app/src/main/java/com/hich2000/tagcapella/Database.kt
@@ -0,0 +1,32 @@
+package com.hich2000.tagcapella
+
+import android.app.Application
+import androidx.sqlite.db.SupportSQLiteDatabase
+import app.cash.sqldelight.db.SqlDriver
+import app.cash.sqldelight.driver.android.AndroidSqliteDriver
+import com.hich200.tagcapella.TagcapellaDb
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class Database @Inject constructor(
+    application: Application
+) {
+
+    private var _db: TagcapellaDb
+    val db: TagcapellaDb get() = _db
+
+    init {
+        val driver: SqlDriver = AndroidSqliteDriver(
+            TagcapellaDb.Schema,
+            application,
+            "tagcapella.db",
+            callback = object : AndroidSqliteDriver.Callback(TagcapellaDb.Schema) {
+                override fun onOpen(db: SupportSQLiteDatabase) {
+                    db.setForeignKeyConstraintsEnabled(true)
+                }
+            })
+        _db = TagcapellaDb(driver)
+    }
+
+}
--- app/src/main/java/com/hich2000/tagcapella/tags/TagViewModel.kt
+++ app/src/main/java/com/hich2000/tagcapella/tags/TagViewModel.kt
@@ -1,40 +1,26 @@
 package com.hich2000.tagcapella.tags
 
-import android.app.Application
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.runtime.toMutableStateList
 import androidx.lifecycle.ViewModel
-import androidx.sqlite.db.SupportSQLiteDatabase
-import app.cash.sqldelight.db.SqlDriver
-import app.cash.sqldelight.driver.android.AndroidSqliteDriver
-import com.hich200.tagcapella.TagcapellaDb
 import com.hich200.tagcapella.Tag
+import com.hich200.tagcapella.TagcapellaDb
+import com.hich2000.tagcapella.Database
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class TagViewModel @Inject constructor(
-    application: Application
+    database: Database
 ) : ViewModel() {
 
     private var _tags = mutableStateListOf<Tag>()
     val tags: SnapshotStateList<Tag> get() = _tags
 
-    private var db: TagcapellaDb
+    private var db: TagcapellaDb = database.db
 
     init {
-        val driver: SqlDriver = AndroidSqliteDriver(
-            TagcapellaDb.Schema,
-            application,
-            "tagcapella.db",
-            callback = object : AndroidSqliteDriver.Callback(TagcapellaDb.Schema) {
-                override fun onOpen(db: SupportSQLiteDatabase) {
-                    db.setForeignKeyConstraintsEnabled(true)
-                }
-            })
-        db = TagcapellaDb(driver)
-
         _tags = selectAllTags()
     }
 
