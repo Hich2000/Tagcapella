989658cbae2bf3817c715f7f8ad5576eb8206360
Hich2000 <33690542+Hich2000@users.noreply.github.com>
2025-09-02 19:52:09 +0200

    added a category dto and repository

---
+++ app/src/main/java/com/hich2000/tagcapella/tags/CategoryRepository.kt
@@ -0,0 +1,35 @@
+package com.hich2000.tagcapella.tags
+
+import com.hich200.tagcapella.TagcapellaDb
+import com.hich2000.tagcapella.utils.Database
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class CategoryRepository @Inject constructor(
+    private val database: Database
+) {
+
+    private val db: TagcapellaDb = database.db
+
+    fun selectAllCategories(): List<CategoryDTO> {
+        return db.categoryQueries.selectAll { id, category -> CategoryDTO(id, category, database) }.executeAsList()
+            .toList()
+    }
+
+    fun insertCategories(newCategory: String): CategoryDTO {
+        db.categoryQueries.insertCategory(newCategory)
+        return db.categoryQueries.lastInsertedCategory { id, category -> CategoryDTO(id, category, database) }
+            .executeAsOne()
+    }
+
+    fun updateCategories(id: Long, category: String) {
+        db.categoryQueries.updateCategory(category, id)
+        //todo add something here to check if the update was successful
+    }
+
+    fun deleteCategories(id: Long) {
+        db.categoryQueries.deleteCategory(id)
+        //todo add something here to check if the delete was successful
+    }
+}
