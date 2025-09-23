49a09cc95a99da7cd24d741c39dd2972ed130498
Hich2000 <33690542+Hich2000@users.noreply.github.com>
2025-09-23 20:56:47 +0200

    duplicate tags no longer crash the app and show a toast message instead.

--- app/src/main/java/com/hich2000/tagcapella/tags/TagViewModel.kt
+++ app/src/main/java/com/hich2000/tagcapella/tags/TagViewModel.kt
@@ -1,10 +1,13 @@
 package com.hich2000.tagcapella.tags
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.hich2000.tagcapella.songs.Song
+import com.hich2000.tagcapella.utils.ToastEventBus
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -24,13 +27,25 @@ class TagViewModel @Inject constructor(
     }
 
     fun insertTag(tag: String, category: Long?) {
-        tagRepository.insertTag(newTag = tag, category = category)
-        _tags.value = selectAllTags()
+        viewModelScope.launch {
+            try {
+                tagRepository.insertTag(newTag = tag, category = category)
+                _tags.value = selectAllTags()
+            } catch (_: Throwable) {
+                ToastEventBus.send("Tag already exists with name: $tag")
+            }
+        }
     }
 
     fun updateTag(id: Long, tag: String, category: Long?) {
-        tagRepository.updateTag(id = id, tag = tag, category = category)
-        _tags.value = selectAllTags()
+        viewModelScope.launch {
+            try {
+                tagRepository.updateTag(id = id, tag = tag, category = category)
+                _tags.value = selectAllTags()
+            } catch (_: Throwable) {
+                ToastEventBus.send("Tag already exists with name: $tag")
+            }
+        }
     }
 
     fun deleteTag(id: Long) {
