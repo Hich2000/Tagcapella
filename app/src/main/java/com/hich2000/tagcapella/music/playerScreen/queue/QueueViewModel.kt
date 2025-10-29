c94d9937cb05b075964a5a77ca1bb8522248e497
Hich2000 <33690542+Hich2000@users.noreply.github.com>
2025-10-29 21:57:05 +0100

    when selecting a song from the queue it now also starts playback if the mediaplayer was paused.

--- app/src/main/java/com/hich2000/tagcapella/music/playerScreen/queue/QueueViewModel.kt
+++ app/src/main/java/com/hich2000/tagcapella/music/playerScreen/queue/QueueViewModel.kt
@@ -2,6 +2,7 @@ package com.hich2000.tagcapella.music.playerScreen.queue
 
 import androidx.lifecycle.ViewModel
 import com.hich2000.tagcapella.music.mediaController.MediaPlayerCoordinator
+import com.hich2000.tagcapella.music.playerState.PlayerState
 import com.hich2000.tagcapella.music.queueManager.Song
 import com.hich2000.tagcapella.tagsAndCategories.tags.Tag
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -20,10 +21,14 @@ class QueueViewModel @Inject constructor(
     val currentQueue: StateFlow<List<Song>> get() = mediaPlayerCoordinator.currentQueue
     val includedTags: StateFlow<List<Tag>> get() = mediaPlayerCoordinator.includedTags
     val excludedTags: StateFlow<List<Tag>> get() = mediaPlayerCoordinator.excludedTags
+    val playerState: StateFlow<PlayerState> get() = mediaPlayerCoordinator.playerState
 
     fun seek(song: Song) {
         val index = currentQueue.value.indexOf(song)
         mediaPlayerCoordinator.seek(index)
+        if (!playerState.value.isPlaying) {
+            mediaPlayerCoordinator.play()
+        }
     }
 
     fun openDialog() {
