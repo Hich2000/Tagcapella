276005dffcebcbcd8c64d3bbc3a57c907ef74c7b
Hich2000 <33690542+Hich2000@users.noreply.github.com>
2025-12-01 21:04:51 +0100

    removed some paddings, reverted the animated bottom nav, because it was more annoying. Gonna have to figure out multiple navhosts and navigation between them.

--- app/src/main/java/com/hich2000/tagcapella/main/TagcapellaApp.kt
+++ app/src/main/java/com/hich2000/tagcapella/main/TagcapellaApp.kt
@@ -2,16 +2,11 @@ package com.hich2000.tagcapella.main
 
 import android.widget.Toast
 import androidx.compose.animation.AnimatedContentTransitionScope
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.EnterTransition
 import androidx.compose.animation.core.tween
-import androidx.compose.animation.slideInHorizontally
-import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -32,7 +27,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavType
@@ -79,18 +73,7 @@ fun TagcapellaApp() {
         modifier = Modifier
             .fillMaxSize(),
         bottomBar = {
-            AnimatedVisibility(
-                visible = showNavBar,
-                enter = if (firstComposition) EnterTransition.None
-                else slideInHorizontally(
-                    animationSpec = tween(slideSpeed),
-                    initialOffsetX = { -it }
-                ),
-                exit = slideOutHorizontally(
-                    animationSpec = tween(slideSpeed),
-                    targetOffsetX = { -it }
-                )
-            ) {
+            if (showNavBar) {
                 BottomNavBar(mainNavController)
             }
         },
@@ -130,11 +113,7 @@ fun TagcapellaApp() {
     ) { innerPadding ->
         Box(
             modifier = Modifier
-                .padding(
-                    top = innerPadding.calculateTopPadding(),
-                    start = if (showNavBar) innerPadding.calculateStartPadding(LocalLayoutDirection.current) else 0.dp,
-                    bottom = if (showNavBar) innerPadding.calculateBottomPadding() else 0.dp
-                )
+                .padding(innerPadding)
                 .fillMaxSize()
         ) {
             CompositionLocalProvider(LocalNavController provides mainNavController) {
--- app/src/main/java/com/hich2000/tagcapella/tagsAndCategories/tags/tagList/TagList.kt
+++ app/src/main/java/com/hich2000/tagcapella/tagsAndCategories/tags/tagList/TagList.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -12,7 +11,6 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -20,7 +18,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
@@ -41,79 +38,74 @@ fun TagList(
     //funny little bug prevention method :^ )
     tagListViewModel.verifySelectedCategory()
 
-    Scaffold { innerPadding ->
-        Column(
-            modifier = Modifier
-                .padding(
-                    start = innerPadding.calculateStartPadding(LocalLayoutDirection.current)
-                )
-                .fillMaxSize()
-                .verticalScroll(columnScroll)
-        ) {
-            if (categories.isNotEmpty()) {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(columnScroll)
+    ) {
+        if (categories.isNotEmpty()) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .horizontalScroll(scroll)
+            ) {
+                TagCapellaButton(
+                    onClick = {
+                        tagListViewModel.setSelectedCategory(null)
+                    },
                     modifier = Modifier
-                        .fillMaxWidth()
-                        .horizontalScroll(scroll)
+                        .border(2.dp, MaterialTheme.colorScheme.tertiary, RectangleShape)
+                        .padding(0.dp)
+                        .width(120.dp),
+                    shape = RectangleShape,
                 ) {
+                    Text(
+                        text = "All",
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 8.dp),
+                    )
+                }
+
+                categories.forEach { category ->
+                    val buttonModifier = Modifier
+                        .border(2.dp, MaterialTheme.colorScheme.tertiary, RectangleShape)
+                        .padding(0.dp)
+                    val finalModifier = if (category.category.length < 20) {
+                        buttonModifier.width(120.dp)
+                    } else {
+                        buttonModifier.wrapContentWidth()
+                        buttonModifier.weight(1f)
+                    }
+
                     TagCapellaButton(
                         onClick = {
-                            tagListViewModel.setSelectedCategory(null)
+                            tagListViewModel.setSelectedCategory(category.id)
                         },
-                        modifier = Modifier
-                            .border(2.dp, MaterialTheme.colorScheme.tertiary, RectangleShape)
-                            .padding(0.dp)
-                            .width(120.dp),
+                        modifier = finalModifier,
                         shape = RectangleShape,
                     ) {
                         Text(
-                            text = "All",
+                            text = category.category,
                             textAlign = TextAlign.Center,
                             modifier = Modifier
                                 .fillMaxWidth()
                                 .padding(horizontal = 8.dp),
                         )
                     }
-
-                    categories.forEach { category ->
-                        val buttonModifier = Modifier
-                            .border(2.dp, MaterialTheme.colorScheme.tertiary, RectangleShape)
-                            .padding(0.dp)
-                        val finalModifier = if (category.category.length < 20) {
-                            buttonModifier.width(120.dp)
-                        } else {
-                            buttonModifier.wrapContentWidth()
-                            buttonModifier.weight(1f)
-                        }
-
-                        TagCapellaButton(
-                            onClick = {
-                                tagListViewModel.setSelectedCategory(category.id)
-                            },
-                            modifier = finalModifier,
-                            shape = RectangleShape,
-                        ) {
-                            Text(
-                                text = category.category,
-                                textAlign = TextAlign.Center,
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(horizontal = 8.dp),
-                            )
-                        }
-                    }
                 }
-            }
-
-
-            tagList.forEach { tag ->
-                if (selectedCategory !== null && tag.categoryId != selectedCategory) {
-                    return@forEach
-                }
-                tagCard(tag)
             }
         }
 
+
+        tagList.forEach { tag ->
+            if (selectedCategory !== null && tag.categoryId != selectedCategory) {
+                return@forEach
+            }
+            tagCard(tag)
+        }
     }
 }
