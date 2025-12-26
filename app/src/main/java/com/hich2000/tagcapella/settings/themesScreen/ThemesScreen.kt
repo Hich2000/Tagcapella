a1e6aa8f7a5b32729a394ae4da096905725110a5
Hich2000 <33690542+Hich2000@users.noreply.github.com>
2025-12-26 18:19:47 +0100

    added new route and placeholder screen

---
+++ app/src/main/java/com/hich2000/tagcapella/settings/themesScreen/ThemesScreen.kt
@@ -0,0 +1,12 @@
+package com.hich2000.tagcapella.settings.themesScreen
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+
+@Composable
+fun ThemesScreen(
+    themesScreenViewModel: ThemesScreenViewModel = hiltViewModel()
+) {
+    Text("ayylmao")
+}
---
+++ app/src/main/java/com/hich2000/tagcapella/settings/themesScreen/ThemesScreenViewModel.kt
@@ -0,0 +1,9 @@
+package com.hich2000.tagcapella.settings.themesScreen
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ThemesScreenViewModel @Inject constructor() : ViewModel() {
+}
