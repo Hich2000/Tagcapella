9d622295e23e73711137d4f6c2bb11017f0ab491
Hich2000 <33690542+Hich2000@users.noreply.github.com>
2026-01-02 14:28:59 +0100

    added a soundwave icon to songcard that is currently playing in queue

--- app/src/main/java/com/hich2000/tagcapella/music/playerScreen/queue/Queue.kt
+++ app/src/main/java/com/hich2000/tagcapella/music/playerScreen/queue/Queue.kt
@@ -1,7 +1,12 @@
 package com.hich2000.tagcapella.music.playerScreen.queue
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Queue
@@ -12,10 +17,13 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import com.hich2000.tagcapella.R
 import com.hich2000.tagcapella.main.navigation.LocalNavController
 import com.hich2000.tagcapella.main.navigation.Route
 import com.hich2000.tagcapella.music.songScreen.SongCard
@@ -27,6 +35,7 @@ fun Queue(
 ) {
     val navController = LocalNavController.current
     val queue by queueViewModel.currentQueue.collectAsState()
+    val playerState by queueViewModel.playerState.collectAsState()
 
     Scaffold(
         floatingActionButton = {
@@ -51,10 +60,33 @@ fun Queue(
             songList = queue,
             modifier = Modifier.padding(innerPadding)
         ) { song ->
-            SongCard(
-                song = song,
-                onClick = { queueViewModel.seek(song) }
-            )
+            if (playerState.currentSong == song.path) {
+                Box(
+                    contentAlignment = Alignment.Center,
+                ) {
+                    SongCard(
+                        song = song,
+                        onClick = { queueViewModel.seek(song) }
+                    )
+                    Row(
+                        horizontalArrangement = Arrangement.End,
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.soundwave),
+                            contentDescription = "Sound wave",
+                            modifier = Modifier
+                                .padding(end = 8.dp)
+                                .background(MaterialTheme.colorScheme.primary)
+                        )
+                    }
+                }
+            } else {
+                SongCard(
+                    song = song,
+                    onClick = { queueViewModel.seek(song) }
+                )
+            }
         }
     }
 }
